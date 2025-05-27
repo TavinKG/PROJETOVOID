4c19ad1e44771cfa45b404ba136ec44c5d481e0e
Otávio <otaviomarcal785@gmail.com>
2025-05-26 21:44:59 -0300

    new database

--- backend/config/supabase.js
+++ backend/config/supabase.js
@@ -1,7 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const SUPABASE_URL = 'https://jqgdvfxtoijpkrcrbknn.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxZ2R2Znh0b2lqcGtyY3Jia25uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI0MjYyNTEsImV4cCI6MjA0ODAwMjI1MX0.INe9XNxOwbocuct19KTYdfNZLG9e1jD2HDQUrXMS0zM';
+const SUPABASE_URL = 'https://dgkohzgisnqwhymnsriu.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRna29oemdpc25xd2h5bW5zcml1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxOTI0MzgsImV4cCI6MjA2Mzc2ODQzOH0.3LESBnBTRHAzEyBd6b-KtWOGELqAyKVHBL79JvB2Emk';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
